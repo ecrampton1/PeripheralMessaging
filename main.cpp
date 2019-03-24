5dc97b7fd326ddf306b2d926132f9a9da44ec389
Ed Crampton <eddie.crampton@gmail.com>
2019-03-24 13:41:54 +0000

    adding niceness to program, note this now requires this to be run as root.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
+ #include <sys/time.h>
+#include <sys/resource.h>
 #include "mqtt_rfm69_bridge.hpp"
 
 
 int main()
 {	
+	//This needs access to GPIO and SPI running at normal niceness 
+	//has shown to lose packets over rf network on RPI.
+	setpriority(PRIO_PROCESS, 0, -20);
+	
 	MqttRfm69Bridge bridge;
 
 	while(1)
